a8844ca18f61a32d7c9e735a79d3866e4b1c6bfe
Sheldon Jones <sheldonajones@yahoo.com>
2025-12-05 15:56:33 -0500

    js file and button functionality added

--- world.php
+++ world.php
@@ -9,9 +9,23 @@ $stmt = $conn->query("SELECT * FROM countries");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+$country = isset($_GET['country']) ? $_GET['country'] : '';
+
+if (!empty($country)) {
+    
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+} else {
+    
+    $stmt = $conn->query("SELECT * FROM countries");
+}
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 <ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
 </ul>
+
+
