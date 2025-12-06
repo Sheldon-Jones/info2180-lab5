08a7c9bb40475be79a605e3b423eb0996dfe03bc
Sheldon Jones <sheldonajones@yahoo.com>
2025-12-05 20:19:41 -0500

    table inserted in world.php

--- world.php
+++ world.php
@@ -7,25 +7,83 @@ $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $country = isset($_GET['country']) ? $_GET['country'] : '';
 
-if (!empty($country)) {
-    
-    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-} else {
-    
-    $stmt = $conn->query("SELECT * FROM countries");
-}
+$lookup = isset($_GET ['lookup']) ? $_GET['lookup'] : 'countries';
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+if ($lookup === 'cities') {
+    
+    if (!empty($country)) {
+        $query = "SELECT cities.name as city_name, cities.district, cities.population FROM cities JOIN countries 
+        ON cities.country_code = countries.code WHERE countries.name LIKE '%$country%'";
+        $stmt = $conn->query($query);
+    } 
+    else {
+        
+        $stmt = $conn->query("SELECT cities.name as city_name, cities.district, cities.population FROM cities");
+    }
+    
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    ?>
+
+<table border="1">
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Continent</th>
+            <th>Independence</th>
+            <th>Head of State</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($results as $row): ?>
+        <tr>
+            <td><?= htmlspecialchars($row['name']) ?></td>
+            <td><?= htmlspecialchars($row['continent']) ?></td>
+            <td><?= htmlspecialchars($row['independence_year']) ?></td>
+            <td><?= htmlspecialchars($row['head_of_state']) ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+  <?php
+  } 
+  
+  else {
+    
+    if (!empty($country)) {
+        $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    } else {
+        $stmt = $conn->query("SELECT * FROM countries");
+    }
+    
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    ?>
+
+    <table border="1">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Continent</th>
+          <th>Independence</th>
+           <th>Head of State</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($results as $row): ?>
+          <tr>
+            <td><?= htmlspecialchars($row['name']) ?></td>
+            <td><?= htmlspecialchars($row['continent']) ?></td>
+            <td><?= htmlspecialchars($row['independence_year']) ?></td>
+            <td><?= htmlspecialchars($row['head_of_state']) ?></td>
+          </tr>
 <?php endforeach; ?>
-</ul>
-
+        </tbody>
+    </table>
+    <?php
+}
+?>
 
